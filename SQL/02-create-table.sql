48d9113b38c019b1ff96acbe23fadcfbade195a4
verba-neo <verba.it.corp@gmail.com>
2025-06-30 14:20:32 +0900

    CREATE TABLE

--- SQL/02-create-table.sql
+++ SQL/02-create-table.sql
@@ -1,8 +1,31 @@
 -- 02-create-table.sql
+USE lecture;
 
+-- 테이블 생성
 CREATE TABLE sample (
 	name VARCHAR(30),
     age INT
 );
 
+-- 테이블 삭제
+DROP TABLE sample;
+
+-- 테이블 확인
 SHOW TABLES;
+
+CREATE TABLE members (  -- 테이블 명: memebers 생성
+	id INT AUTO_INCREMENT PRIMARY KEY,  -- 회원 고유번호 (정수, 자동증가)
+    name VARCHAR(30) NOT NULL,  -- 이름(필수 입력)
+    email VARCHAR(100) UNIQUE,  -- 이메일(중복 불가능) 
+    join_date DATE DEFAULT(CURRENT_DATE)  -- 가입일(기본값-오늘)
+    
+);
+
+SHOW TABLES;
+
+-- members 테이블을 상세히 확인 (Describe)
+DESC members;
+
+
+
+
---
+++ SQL/p01.sql
@@ -0,0 +1,10 @@
+-- p01.sql
+
+-- 1. practice db 사용
+-- 2. userinfo 테이블 생성
+	-- id PK, auto inc, int
+    -- nickname: 글자 20자 까지, 필수 입력
+    -- phone 글자 11 글자 까지, 중복 방지
+    -- reg_date 날짜, 기본값(오늘 날짜)
+    
+-- 3. desc 로 테이블 정보 확인
