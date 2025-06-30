62fda005d87efb546c089ef696553c0a62f9d415
verba-neo <verba.it.corp@gmail.com>
2025-06-30 16:23:38 +0900

    p03

---
+++ SQL/p03.sql
@@ -0,0 +1,11 @@
+-- p03.sql
+
+-- practice db 사용
+
+-- 스키마 확인 & 데이터 확인 (주기적으로)
+
+-- userinfo 에 email 컬럼 추가 40글자 제한, 중복 안됨, 기본값은 ex@gmail.com
+-- nickname 길이제한 100자로 늘리기
+-- reg_date 컬럼 삭제
+-- 실제 한명의 email 을 수정하기
+
