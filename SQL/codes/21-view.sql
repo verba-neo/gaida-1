3c5c5d99649b0f7061eea522b4f76e1f479c9c1d
verba-neo <verba.it.corp@gmail.com>
2025-07-04 16:52:56 +0900

    answer of making view

--- SQL/codes/21-view.sql
+++ SQL/codes/21-view.sql
@@ -35,15 +35,43 @@ LIMIT 10;
 
 
 
--- View 2: 카테고리별 성과 요약 View
+-- View 2: 카테고리별 성과 요약 View (category_performance)
 -- 카테고리 별로, 총 주문건수, 총매출액, 평균주문금액, 구매고객수, 판매상품수, 매출비중(전체매출에서 해당 카테고리가 차지하는비율)
 
+CREATE VIEW category_performance AS
+SELECT
+    s.category,
+    COUNT(*) AS 총주문건수,
+    SUM(s.total_amount) AS 총매출액,
+    AVG(s.total_amount) AS 평균주문금액,
+    COUNT(DISTINCT s.customer_id) AS 구매고객수,
+    COUNT(DISTINCT s.product_name) AS 판매상품수,
+    ROUND(SUM(s.total_amount) * 100.0 / (SELECT SUM(total_amount) FROM sales), 2) AS 매출비중
+FROM sales s
+GROUP BY s.category;
+
+SELECT * FROM category_performance;
 
 
-
--- View 3: 월별 매출 요약
+-- View 3: 월별 매출 요약 (monthly_sales )
 -- 년월(24-07), 월주문건수, 월평균매출액, 월활성고객수, 월신규고객수
 
+CREATE VIEW monthly_sales AS
+SELECT
+    DATE_FORMAT(s.order_date, '%Y-%m') AS 년월,
+    COUNT(*) AS 월주문건수,
+    SUM(s.total_amount) AS 월매출액,
+    AVG(s.total_amount) AS 월평균주문액,
+    COUNT(DISTINCT s.customer_id) AS 월활성고객수,
+    COUNT(DISTINCT c.customer_id) AS 월신규고객수
+FROM sales s
+LEFT JOIN customers c
+    ON s.customer_id = c.customer_id
+    AND DATE_FORMAT(s.order_date, '%Y-%m') = DATE_FORMAT(c.join_date, '%Y-%m')
+GROUP BY
+    DATE_FORMAT(s.order_date, '%Y-%m');
+
+SELECT * FROM monthly_sales;
 
 
 
