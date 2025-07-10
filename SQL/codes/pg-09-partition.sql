622b47d88d00dd8089f2d1bc0f1c12e35c41fb9f
verba-neo <verba.it.corp@gmail.com>
2025-07-10 11:43:26 +0900

    partition

--- SQL/codes/pg-09-partition.sql
+++ SQL/codes/pg-09-partition.sql
@@ -50,8 +50,7 @@ FROM daily_sales;
 WITH daily_sales AS (
 	SELECT
 		order_date,
-		SUM(amount) AS 일매출,
-		COUNT(*) AS 주문수
+		SUM(amount) AS 일매출
 	FROM orders
 	WHERE order_date BETWEEN '2024-06-01' AND '2024-08-31'
 	GROUP BY order_date
@@ -59,17 +58,66 @@ WITH daily_sales AS (
 )
 SELECT
 	order_date,
-	일매출,
-	주문수,
+	일매출,	
 	ROUND(AVG(일매출) OVER(
 		ORDER BY order_date
 		ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
 	)) AS 이동평균7일,
 	ROUND(AVG(일매출) OVER(
 		ORDER BY order_date
-		ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+		ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
 	)) AS 이동평균3일
 FROM daily_sales;
+
+
+-- 카테고리 별 인기 상품(매출순위) TOP 5
+-- CTE
+-- [상품 카테고리, 상품id, 상품이름, 상품가격, 해당상품의주문건수, 해당상품판매개수, 해당상품총매출]
+-- 위에서 만든 테이블에 WINDOW함수 컬럼추가 + [매출순위, 판매량순위]
+-- 총데이터 표시(매출순위 1 ~ 5위 기준으로 표시)
+WITH product_sales AS (
+	SELECT
+		p.category,
+		p.product_id,
+		p.product_name,
+		p.price,
+		COUNT(o.order_id) AS 주문건수,
+		SUM(o.quantity) AS 판매개수,
+		SUM(o.amount) AS 총매출
+	FROM products p
+	LEFT JOIN orders o ON p.product_id = o.product_id
+	GROUP BY p.category, p.product_id, p.product_name, p.price
+),
+ranked_products AS (
+	SELECT
+		*,
+		DENSE_RANK() OVER (PARTITION BY category ORDER BY 총매출 DESC) AS 매출순위,
+		DENSE_RANK() OVER (PARTITION BY category ORDER BY 판매개수 DESC) AS 판매량순위
+	FROM product_sales
+)
+SELECT
+	category, product_name, price, 주문건수, 판매개수, 총매출, 매출순위, 판매량순위
+FROM ranked_products
+WHERE 매출순위 <= 5
+ORDER BY category, 매출순위;
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 SELECT
