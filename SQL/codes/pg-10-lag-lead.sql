ed8fa1103a2f08d22c8358f9b035fed41352de97
verba-neo <verba.it.corp@gmail.com>
2025-07-10 14:55:00 +0900

    window 실습

--- SQL/codes/pg-10-lag-lead.sql
+++ SQL/codes/pg-10-lag-lead.sql
@@ -43,15 +43,56 @@ WHERE customer_id='CUST-000001'
 ORDER BY customer_id, order_date
 
 -- [고객id, 주문일, 금액, 구매 순서(ROW_NUMBER),
---	이전구매간격, 다음구매간격
+-- 이전구매간격, 다음구매간격
 -- 금액변화=(이번-저번), 금액변화율
 -- 누적 구매 금액(SUM OVER)
 -- [추가]누적 평균 구매 금액 (AVG OVER)
 -- ]
 
-
-
-
+WITH customer_orders AS (
+	SELECT
+		ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS 구매순서,
+		customer_id,
+		amount,
+		LAG(amount, 1) OVER (PARTITION BY customer_id ORDER BY order_date) AS 이전구매금액,
+		LAG(order_date, 1) OVER (PARTITION BY customer_id ORDER BY order_date) AS 이전구매일,
+		order_date,
+		LEAD(order_date, 1) OVER (PARTITION BY customer_id ORDER BY order_date) AS 다음구매일
+	FROM orders
+)
+SELECT
+	customer_id,
+	order_date,
+	amount,
+	구매순서,
+	-- 구매 간격
+	order_date - 이전구매일 AS 이전구매간격,
+	다음구매일 - order_date AS 다음구매간격,
+	-- 구매금액변화
+	amount - 이전구매금액 AS 금액변화,
+	CASE
+		WHEN 이전구매금액 IS NULL THEN NULL
+		ELSE ROUND((amount - 이전구매금액) * 100 / 이전구매금액, 2)::TEXT || '%'
+	END AS 금액변화율,
+	--  누적 구매 통계
+	SUM(amount) OVER (PARTITION BY customer_id ORDER BY order_date) AS 누적구매금액,
+	AVG(amount) OVER (
+		PARTITION BY customer_id
+		ORDER BY order_date
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW  -- 현재 확인중인 ROW 부터 맨 앞까지
+		-- ROWS BETWEEN 2 PRECEDING AND CURRENT ROW  -- 현재 확인중인 ROW 포함 총 3개
+	) AS 평균구매금액,
+	-- 고객 구매 단계 분류
+	CASE
+		WHEN 구매순서 = 1 THEN '첫구매'
+		WHEN 구매순서 <= 3 THEN '초기고객'
+		WHEN 구매순서 <= 10 THEN '일반고객'
+		ELSE 'VIP고객'
+	END AS 고객단계
+FROM customer_orders
+ORDER BY customer_id, order_date;
+	
+	
 
 
 
