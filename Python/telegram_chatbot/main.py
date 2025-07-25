552cb6c4173524874bc1f68039ef95bf9f6e4a6d
verba-neo <verba.it.corp@gmail.com>
2025-07-25 17:48:29 +0900

    gg

--- Python/telegram_chatbot/main.py
+++ Python/telegram_chatbot/main.py
@@ -4,36 +4,52 @@
 pip install fastapi
 pip install uvicorn[standard]
 
-이후 아래 터미널 명령어로 서버 켬
+터미널에서 현재 파일의 위치로 이동(cd) 이후 명령어로 서버 켬
 uvicorn main:app --reload
 '''
-
-from fastapi import FastAPI
-import random
+from fastapi import FastAPI, Request
 import requests
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
 
+# .env 파일을 불러옴
+load_dotenv()
 app = FastAPI()
 
 
-@app.get('/hi')
-def hi():
-    return {'status': '굿'}
+def send_message(chat_id, message):
+    # .env 에서 'TELEGRAM_BOT_TOKEN'에 해당하는 값을 불러옴
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 
-
-@app.get('/lotto')
-def lotto():
-    return {
-        'numbers': random.sample(range(1, 46), 6)
-    }
-
-
-@app.get('/gogogo')
-def gogogo():
-    bot_token = '8094231169:AAGEmcVeMvUSbhpbHmJPT0dgKx7NjstQtQ8'
     URL = f'https://api.telegram.org/bot{bot_token}'
     body = {
-        'chat_id': '765946187',
-        'text': '이 메시지는 서버가 보냄', 
+        # 사용자 chat_id 는 어디서 가져옴..?
+        'chat_id': chat_id,
+        'text': message,
     }
     requests.get(URL + '/sendMessage', body)
-    return {'status': 'gogogo'}
+    
+
+# /telegram 라우팅으로 텔레그램 서버가 Bot에 업데이트가 있을 경우, 우리에게 알려줌
+@app.post('/telegram')
+async def telegram(request: Request):
+    print('텔레그램에서 요청이 들어왔다!!!!')
+    
+    data = await request.json()
+    print(data)
+    sender_id = data['message']['chat']['id']
+    input_msg = data['message']['text']
+
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+    
+    res = client.responses.create(
+        model='gpt-4.1-mini',
+        input=input_msg,
+        instructions='너는 초등학교 선생님이야. 모든 질문에 친절하게 설명해줘.',
+        temperature=0
+    )
+
+    send_message(sender_id, res.output_text)
+
+    return {'status': '굿'}
