5190dab31cb0ee256e95a4ff5158eed63818aae8
verba-neo <verba.it.corp@gmail.com>
2025-09-12 17:48:08 +0900

    final

--- LLM-agent/15_code_interpreter/state.py
+++ LLM-agent/15_code_interpreter/state.py
@@ -6,9 +6,9 @@ from typing_extensions import Any
 class State(MessagesState):
     question: str        # 사용자 질문
     db_status: bool      # db에서 해당 정보를 찾을 수 있는지 없는지
-    db_reason: str       # status 결정 이유
+    db_reason: str       # db_status 결정 이유
     sql: str             # question을 변환한 SQL
-    sql_result: Any      # DB에서 받은 결과
+    sql_result: Any      # `sql`의 실행결과
     code: str            # 파이썬 코드 블럭
     code_result: str     # `code`의 실행결과
     answer: str          # 최종 답변
